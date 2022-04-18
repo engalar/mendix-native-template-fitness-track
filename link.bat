483dd35d979a784fdef9371ed93d7fdc48ab2b13
engalar <engalar@gmail.com>
2022-04-18 08:32:58 +0800

    wip

--- link.bat
+++ link.bat
@@ -1,7 +1,12 @@
-set list=@react-native-community/netinfo react-native-action-button react-native-device-info react-native-amap3d react-native-image-picker react-native-localize 
+@REM JavaScript action native dependency
+npm i @mauron85/react-native-background-geolocation
+@REM patch node_modules\@mauron85\react-native-background-geolocation\android\common\gradle.properties
+
+@REM manual link
+set list=@react-native-community/netinfo react-native-action-button react-native-device-info react-native-amap3d react-native-image-picker react-native-localize @mauron85/react-native-background-geolocation 
 (for %%a in (%list%) do ( 
    npx react-native link %%a 
 ))
 
-npm i @mauron85/react-native-background-geolocation
-npx react-native link @mauron85/react-native-background-geolocation
+@REM install apk into live device
+npx react-native run-android --variant=DevDebug
